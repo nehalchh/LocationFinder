6dd889f5d15efe63f3b794889c909dabbe3912a7
Nehalchh <nehalchhalotre27@gmail.com>
2024-09-25 02:36:50 +0530

    Location Finder App

--- LocationFinder/src/App.jsx
+++ LocationFinder/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import ReactMapGL, { Marker, FlyToInterpolator }
+      from "react-map-gl";
+import Fly from "./Components/Fly";
+import { ImLocation } from "react-icons/im";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // Setting up the state for the latitude
+  // and longitude
+  const [lat, setLat] = useState(22.5726);
+  const [lon, setLon] = useState(88.3639);
+
+  // Setting up the state for the map
+  const [viewport, setViewport] = useState({
+    latitude: lat,
+    longitude: lon,
+    zoom: 12,
+    bearing: 0,
+    pitch: 0,
+    width: "100%",
+    height: "100vh",
+  });
+
+  // Map viewport updates whenever the
+  // latitude and longitude changes
+  useEffect(() => {
+    setViewport({
+      latitude: lat,
+      longitude: lon,
+      zoom: 12,
+      transitionInterpolator: 
+        new FlyToInterpolator({ speed: 1.0 }),
+      transitionDuration: "auto",
+      width: "100%",
+      height: "100vh",
+    });
+  }, [lat, lon]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ReactMapGL
+      mapboxApiAccessToken={"pk.eyJlIjoiYW5vbWljMzAiLCJhIjoiY2tydnFkcTgyMDk5bjJ1bzJhOGRwdHdyYSJ9"}
+      {...viewport}
+      onViewportChange={(viewport) => setViewport(viewport)}
+    >
+      <Marker latitude={lat} longitude={lon}>
+        <ImLocation size="30px" />
+      </Marker>
+      <Fly setLat={setLat} setLon={setLon} />
+    </ReactMapGL>
+  );
 }
 
-export default App
+export default App;
